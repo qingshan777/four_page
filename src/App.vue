5e04f16376c42730f7159d5990a92cfd4a528646
15054854614 <15054854614@163.com>
2021-11-29 11:44:54 +0800

    fix: 修复注释

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: liqingshan
  * @Date: 2021-11-18 09:30:58
- * @LastEditTime: 2021-11-29 11:09:52
+ * @LastEditTime: 2021-11-29 11:44:07
  * @LastEditors: liqingshan
  * @FilePath: \qs-vue-template\src\App.vue
  * @Description:
@@ -9,7 +9,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>Vue3 + Ts</div>
+  <div>Vue3 + Ts + Test</div>
 </template>
 <style>
 @import url('@/style/index.css');
