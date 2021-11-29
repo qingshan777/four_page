b3cc4e0c62bd7e363353ee700fbe07bc80590907
15054854614 <15054854614@163.com>
2021-11-29 11:58:01 +0800

    fix: 去除组件库的样式动态引入插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: liqingshan
  * @Date: 2021-11-17 11:06:42
- * @LastEditTime: 2021-11-29 11:10:20
+ * @LastEditTime: 2021-11-29 11:53:16
  * @LastEditors: liqingshan
  * @FilePath: \qs-vue-template\vite.config.ts
  * @Description:
@@ -9,31 +9,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import styleImport from 'vite-plugin-style-import'
 import WindiCSS from 'vite-plugin-windicss'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    WindiCSS(),
-    styleImport({
-      libs: [
-        {
-          libraryName: 'vant',
-          esModule: true,
-          resolveStyle: (name) => `vant/es/${name}/style`
-        }
-      ]
-    })
-  ],
-  build: {
-    rollupOptions: {
-      input: {
-        index: path.resolve(__dirname, 'src/pages/index/index.html')
-        // index: path.resolve(__dirname, "./index.html"),
-      }
-    }
-  },
+  plugins: [vue(), WindiCSS()],
   resolve: {
     alias: {
       '@': path.resolve('./src')
